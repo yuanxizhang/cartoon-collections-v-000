08043332a7cd4b30c56c62e662747485f1fef7d2
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-04 15:20:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(a)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(a)
+  a.map{|e| e.capialize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
